d3b9ddf99bcc0f17ec9f23589e93aca6669e0b57
Naomi <23469+nomicode@users.noreply.github.com>
2024-07-21 00:37:31 +0000

    Add solution and tests for exercise 5

---
+++ course/ms/csec/tf/101/1/src/test_ex05.py
@@ -0,0 +1,13 @@
+"""Exercise 5: Title Creator"""
+
+
+def title_creator(title, char, times):
+    pass
+
+
+def test_title_creator():
+    pass
+
+
+# Print example output
+print(title_creator("Welcome to Masterschool", "=", 5))
