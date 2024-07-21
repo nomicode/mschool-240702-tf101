505de769429e9afeefba1a8d7f1a6641724d5b0e
Naomi <23469+nomicode@users.noreply.github.com>
2024-07-21 00:39:43 +0000

    Add solution and tests for exercise 10

--- src/test_ex10.py
+++ src/test_ex10.py
@@ -1,11 +1,19 @@
-"""Exercise 10: Middle digit"""
+# Exercise 10: Middle digit
 
 
 def middle_digit(num):
+<<<<<<< HEAD:src/test_ex10.py
     pass
+=======
+    return num
+>>>>>>> 39f7cd7 (Add solution and tests for exercise 10):course/ms/csec/tf/101/1/src/test_ex10.py
 
 
 def test_middle_digit():
+    """Test the middle_digit function."""
+    # Module only used for testing
+    import pytest
+
     pass
 
 
