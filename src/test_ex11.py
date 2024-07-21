6db94ead751829aa93e73fb9644934504e3d33c4
Naomi <23469+nomicode@users.noreply.github.com>
2024-07-21 00:40:49 +0000

    Add solution and tests for exercise 11

--- src/test_ex11.py
+++ src/test_ex11.py
@@ -1,11 +1,19 @@
-"""Exercise 11: Reverse numbers"""
+# Exercise 11: Reverse numbers
 
 
 def reverse_number(number):
+<<<<<<< HEAD:src/test_ex11.py
     pass
+=======
+    return number
+>>>>>>> 5ff4f92 (Add solution and tests for exercise 11):course/ms/csec/tf/101/1/src/test_ex11.py
 
 
 def test_reverse_number():
+    """Test the reverse_number function."""
+    # Module only used for testing
+    import pytest
+
     pass
 
 
