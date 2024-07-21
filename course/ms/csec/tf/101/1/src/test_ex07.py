76d1ab202fe099f7a61cd24ebdcfef8870249db3
Naomi <23469+nomicode@users.noreply.github.com>
2024-07-21 00:38:43 +0000

    Add solution and tests for exercise 7

---
+++ course/ms/csec/tf/101/1/src/test_ex07.py
@@ -0,0 +1,15 @@
+"""Exercise 7: Percent calculation"""
+
+
+def percent(num1, num2):
+    # Your code here
+    pass
+
+
+def test_percent():
+    pass
+
+
+# Print example outputs
+print(percent(155, 500))
+print(percent(250, 125))
