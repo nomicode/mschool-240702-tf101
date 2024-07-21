71f1363a6560a252a8d266e0d5d32c43189f6466
Naomi <23469+nomicode@users.noreply.github.com>
2024-07-21 00:20:58 +0000

    Add solution and tests for exercise 3

--- src/test_ex03.py
+++ src/test_ex03.py
@@ -1,25 +1,8 @@
 # Exercise 3: Concat
 
-# pylint: disable=missing-docstring
-
 
 def special_concat(item_1, item_2, item_3):
-    """Concatenate three items using a special format.
-
-    Specifically:
-    - Items separated by commas
-    - An ampersand before the last item
-    - No Oxford comma
-
-    Args:
-        item_1 (str): First item
-        item_2 (str): Second item
-        item_3 (str): Third item
-    Raises:
-        ValueError: If any argument is not a string
-    Returns:
-        str: Concatenated string
-    """
+    """Concatenate three items using a special format."""
     if not all(isinstance(arg, str) for arg in (item_1, item_2, item_3)):
         raise ValueError("All arguments must be strings")
 
@@ -27,28 +10,34 @@ def special_concat(item_1, item_2, item_3):
 
 
 def test_special_concat():
-    """Tests the special_concat function."""
-    # Avoid importing test module during normal operation
-    # pylint: disable=import-outside-toplevel
+    """Test the special_concat function."""
+    # Module only used for testing
     import pytest
 
+<<<<<<< HEAD:src/test_ex03.py
     # trunk-ignore-all(bandit/B101)
+=======
+>>>>>>> 32b77da (Add solution and tests for exercise 3):course/ms/csec/tf/101/1/src/test_ex03.py
     # Happy path
     assert (
         special_concat("Apple", "Banana", "Cherry") == "Apple, Banana & Cherry"
     )
+
     # Edge cases
     # "Unix was not designed to stop you from doing stupid things, because
     # that would also stop you from doing clever things." - Doug Gwyn
     assert special_concat("", "Banana", "Cherry") == ", Banana & Cherry"
     assert special_concat("Apple", "", "Cherry") == "Apple,  & Cherry"
     assert special_concat("Apple", "Banana", "") == "Apple, Banana & "
+
     # Just for laughs
     assert special_concat("&", "&", "&") == "&, & & &"
-    # Negative case
+
+    # Negative tests
     with pytest.raises(ValueError):
-        # Non-string input
-        special_concat(123, "Banana", "Cherry")
+        # Non-string inputs
+        special_concat("Apple", None, "Cherry")
+        special_concat("Apple", "Banana", None)
 
 
 # Print example output
