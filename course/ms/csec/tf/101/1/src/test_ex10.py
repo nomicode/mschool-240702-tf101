ed9b0eb19875266ce1910670ed3d2a0eb47a7b41
Naomi <23469+nomicode@users.noreply.github.com>
2024-07-21 00:39:43 +0000

    Add solution and tests for exercise 10

---
+++ course/ms/csec/tf/101/1/src/test_ex10.py
@@ -0,0 +1,14 @@
+"""Exercise 10: Middle digit"""
+
+
+def middle_digit(num):
+    pass
+
+
+def test_middle_digit():
+    pass
+
+
+# Print example outputs
+print(middle_digit(234))
+print(middle_digit(902))
