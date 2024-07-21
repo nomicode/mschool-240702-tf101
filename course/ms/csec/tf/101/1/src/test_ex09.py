0adfa5eb2b743823b50aa343594a6f3988796091
Naomi <23469+nomicode@users.noreply.github.com>
2024-07-21 00:39:27 +0000

    Add solution and tests for exercise 9

---
+++ course/ms/csec/tf/101/1/src/test_ex09.py
@@ -0,0 +1,13 @@
+"""Exercise 9: Distance between points"""
+
+
+def euclidean_distance(x1, y1, x2, y2):
+    pass
+
+
+def test_euclidean_distance():
+    pass
+
+
+# Print example output
+print(euclidean_distance(1.5, 3.0, 4.0, 8.0))
