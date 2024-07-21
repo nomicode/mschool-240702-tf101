63d7cc80f5d28c11a619588dac58184153607420
Naomi <23469+nomicode@users.noreply.github.com>
2024-07-21 00:38:26 +0000

    Add solution and tests for exercise 6

---
+++ course/ms/csec/tf/101/1/src/test_ex06.py
@@ -0,0 +1,13 @@
+"""Exercise 6: Coffee Cost"""
+
+
+def coffee_calculator(small, medium, large):
+    pass
+
+
+def test_coffee_calculator():
+    pass
+
+
+# Print example output
+print(coffee_calculator(2, 10, 1))
